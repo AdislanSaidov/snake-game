1d2f6f7dae80c16aa114c4a121f33b1cda685787
tux <user.shadow.man@gmail.com>
2021-10-10 15:21:10 +0300

    fix generating food

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate piston_window;
 
-use piston_window::*;
+use std::collections::HashSet;
 use std::collections::VecDeque;
+use std::hash::{Hash, Hasher};
+
+use piston_window::*;
 use rand::Rng;
 
 const WINDOW_WIDTH: f64 = 600.0;
@@ -19,47 +22,63 @@ enum Direction {
 }
 
 struct Point {
-    x: f64,
-    y: f64,
+    x: i32,
+    y: i32,
 }
 
 struct Game {
     x_delta: f64,
     y_delta: f64,
-    x: f64,
-    y: f64,
+    x: i32,
+    y: i32,
     direction: Direction,
     snake_coords: VecDeque<Point>,
     food_point: Point,
 }
 
-fn generate_food() -> Point {
+fn generate_food(snake_coords: &VecDeque<Point>) -> Point {
     let mut rng = rand::thread_rng();
-    let x = rng.gen_range(0..30) as f64;
-    let y = rng.gen_range(0..30) as f64;
-    return Point { x, y };
+
+    let mut free_coords: Vec<Point> = Vec::new();
+    (0..30).into_iter().for_each(|i| {
+        (0..30).into_iter().for_each(|k| {
+            for p in snake_coords {
+                // println!("x: {} y: {} snake: x: {} y: {}", i, k, p.x, p.y);
+                if i != p.x && k != p.y {
+                    free_coords.push(Point { x: i, y: k });
+                }
+            }
+        });
+    });
+
+
+    let point_idx = rng.gen_range(0..free_coords.len());
+    return free_coords.remove(point_idx);
 }
 
 impl Game {
     fn new() -> Game {
         let mut deque: VecDeque<Point> = VecDeque::new();
-        deque.push_back(Point { x: 2.0, y: 2.0 });
-        deque.push_back(Point { x: 2.0, y: 3.0 });
-        deque.push_back(Point { x: 2.0, y: 4.0 });
-
+        deque.push_back(Point { x: 2, y: 2 });
+        deque.push_back(Point { x: 2, y: 3 });
+        deque.push_back(Point { x: 2, y: 4 });
+        // (3..980).into_iter().for_each( |i| {
+        //     deque.push_back(Point { x: 2, y: i });
+        // });
+        let food_point = generate_food(&deque);
         Game {
             x_delta: 1.0,
             y_delta: 1.0,
-            x: 1.0,
-            y: 1.0,
+            x: 1,
+            y: 1,
             direction: Direction::NONE,
             snake_coords: deque,
-            food_point: generate_food(),
+            food_point,
         }
     }
 
     fn on_update(&mut self, upd: &UpdateArgs) {
-        let v = (5.0 * upd.dt);
+        let v = 5.0 * upd.dt;
         match self.direction {
             Direction::LEFT => {
                 self.x_delta -= v;
@@ -76,25 +95,26 @@ impl Game {
             Direction::NONE => {}
         }
 
-        if self.x == self.food_point.x && self.y == self.food_point.y {
-            self.food_point = generate_food();
-            let last_idx = self.snake_coords.len() - 1;
-            let last_item = self.snake_coords.get(last_idx).unwrap();
-            self.snake_coords.push_back(Point { x: last_item.x, y: last_item.y })
-        }
-
         if self.direction != Direction::NONE {
             let old_x = self.x;
             let old_y = self.y;
-            self.x = self.x_delta.round();
-            self.y = self.y_delta.round();
+            self.x = self.x_delta.round() as i32;
+            self.y = self.y_delta.round() as i32;
             if old_x == self.x && old_y == self.y {
                 return;
             }
             self.snake_coords.push_front(Point { x: self.x, y: self.y });
             self.snake_coords.pop_back();
-            println!("{}", upd.dt);
-            println!("{}", self.snake_coords.len().to_string());
+
+            // println!("{}", upd.dt);
+            // println!("{}", self.snake_coords.len().to_string());
+        }
+
+        if self.x == self.food_point.x && self.y == self.food_point.y {
+            self.food_point = generate_food(&self.snake_coords);
+            let last_idx = self.snake_coords.len() - 1;
+            let last_item = self.snake_coords.get(last_idx).unwrap();
+            self.snake_coords.push_back(Point { x: last_item.x, y: last_item.y })
         }
     }
 
@@ -108,9 +128,9 @@ impl Game {
 
             let red = [1.0, 0.0, 0.0, 1.0];
             for point in &self.snake_coords {
-                println!("LOOP {} {}", point.x, point.y);
+                // println!("LOOP {} {}", point.x, point.y);
                 let square = rectangle::square(0.0, 0.0, CELL_SIZE);
-                let center = c.transform.trans(point.x * CELL_SIZE, point.y * CELL_SIZE);
+                let center = c.transform.trans((point.x as f64) * CELL_SIZE, (point.y as f64) * CELL_SIZE);
                 rectangle(
                     red,
                     square,
@@ -118,14 +138,15 @@ impl Game {
                     g,
                 );
             }
+            let green = [0.0, 1.0, 0.0, 1.0];
 
             let square = rectangle::square(0.0, 0.0, CELL_SIZE);
             let center = c.transform.trans(
-                self.food_point.x * CELL_SIZE,
-                self.food_point.y * CELL_SIZE
+                (self.food_point.x as f64) * CELL_SIZE,
+                (self.food_point.y as f64) * CELL_SIZE,
             );
             rectangle(
-                red,
+                green,
                 square,
                 center,
                 g,
